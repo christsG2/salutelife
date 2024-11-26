b32a9ac0a8928191c5cc948ae2757f478d65473b
christsG2 <christopherjonesdctg@gmail.com>
2024-11-26 13:40:05 +0100

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://bronsed.co.ua'
+	const proxyUrl = 'https://niostiusc.tech'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const response = await fetch(backendUrl, {
